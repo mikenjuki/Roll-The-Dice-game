d52b80127e996817dcd81ea7de8339ef79c5455f
Michael Njuki <mikenjuki@gmail.com>
2022-12-03 08:56:22 +0300

    Added dice objects and changed default image

--- index.js
+++ index.js
@@ -3,36 +3,58 @@ const bodyContainer = document.querySelector(".body-container");
 const compDiceImg = document.querySelector("#computer-pick img");
 const playerDiceImg = document.querySelector("#player-pick img");
 
-
 const playButton = document.createElement("button");
 playButton.classList.add("play-btn");
 playButton.innerText = "Click to roll";
 bodyContainer.appendChild(playButton);
 
-
 gameIsRunning = false;
 
-// Array of dice images
+// Array of dice images objects
 let diceImages = [
-  "./images/svgs/01-dice.svg",
-  "./images/svgs/02-dice.svg",
-  "./images/svgs/03-dice.svg",
-  "./images/svgs/04-dice.svg",
-  "./images/svgs/05-dice.svg",
-  "./images/svgs/06-dice.svg",
+  {
+    value: 1,
+    imgSrc: "./images/svgs/01-dice.svg",
+  },
+
+  {
+    value: 2,
+    imgSrc: "./images/svgs/02-dice.svg",
+  },
+
+  {
+    value: 3,
+    imgSrc: "./images/svgs/03-dice.svg",
+  },
+
+  {
+    value: 4,
+    imgSrc: "./images/svgs/04-dice.svg",
+  },
+
+  {
+    value: 5,
+    imgSrc: "./images/svgs/05-dice.svg",
+  },
+
+  {
+    value: 6,
+    imgSrc: "./images/svgs/06-dice.svg",
+  },
 ];
+
+console.log(diceImages[0].value);
 
 // Get player choice
 const getPlayerChoice = () => {
   let playerChoiceNum = Math.floor(Math.random() * diceImages.length);
-  playerDiceImg.setAttribute("src", diceImages[playerChoiceNum]);
+  playerDiceImg.setAttribute("src", diceImages[playerChoiceNum].imgSrc);
 };
 
 // Get computer choice
 const getComputerChoice = () => {
   let compChoiceNum = Math.floor(Math.random() * diceImages.length);
-  compDiceImg.setAttribute("src", diceImages[compChoiceNum])
-  
+  compDiceImg.setAttribute("src", diceImages[compChoiceNum].imgSrc);
 };
 
 // Start game by clicking button
@@ -46,7 +68,7 @@ playButton.addEventListener("click", () => {
   console.log("Initializing game sequence...");
 
   getPlayerChoice();
-    getComputerChoice();
+  getComputerChoice();
 
   //   End
   gameIsRunning = false;
